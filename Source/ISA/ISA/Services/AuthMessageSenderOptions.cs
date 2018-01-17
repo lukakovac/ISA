51879fc1b502306c28a4c0c9ee8bb75d23f1c7f8
lkovac <lukakovac153@gmail.com>
2018-01-17 18:23:08 +0100

    add source files

---
+++ Source/ISA/ISA/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISA.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SmtpUser { get; set; }
+        public string SmtpKey { get; set; }
+    }
+}
